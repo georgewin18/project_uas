661073c6360bafee737876a64d3f0033ebf8b23d
george <georgewk1809@gmail.com>
2024-11-11 01:01:34 +0000

    add employees routes

---
+++ server/src/routes/employee.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const EmployeeController = require ('../controller/employee')
+const router = express.Router()
+
+router.get('/', EmployeeController.getAllEmployee)
+router.get('/:id', EmployeeController.getEmployeeById)
+router.get('/department/:id', EmployeeController.getEmployeeByDepartmentId)
+router.get('/job/:id', EmployeeController.getEmployeeByJobId)
+
+router.post('/', EmployeeController.createNewEmployee)
+
+router.patch('/:id', EmployeeController.updateEmployee)
+
+router.delete('/:id', EmployeeController.deleteEmployee)
+
+module.exports = router;
