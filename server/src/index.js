03c1a0e15d6a9f4cbcb2c27cff5d507c5c55165a
george <georgewk1809@gmail.com>
2024-11-11 09:44:40 +0000

    add deployment routes

--- server/src/index.js
+++ server/src/index.js
@@ -6,12 +6,14 @@ const port = process.env.PORT || 8888
 const bodyParser = require('./middleware/bodyparser')
 
 const employeeRoutes = require('./routes/employee')
+const departmentRoutes = require('./routes/department')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded)
 app.use(bodyParser.json)
 
 app.use('/employees', employeeRoutes)
+app.use('/departments', departmentRoutes)
 
 app.listen(port, () => {
   console.log(`Server running in port ${port}`)
