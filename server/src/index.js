a6b16b78261a80be453ae7b67fb91302d3d9dec4
george <georgewk1809@gmail.com>
2024-11-12 21:29:41 +0000

    add cors module

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8888
 const bodyParser = require('./middleware/bodyparser')
+const cors = require('cors')
 
 const employeeRoutes = require('./routes/employee')
 const departmentRoutes = require('./routes/department')
@@ -12,6 +13,7 @@ const attendanceRoutes = require('./routes/attendance')
 const salaryRoutes = require('./routes/salary')
 const leaveAppRoutes = require('./routes/leaveApp')
 
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded)
 app.use(bodyParser.json)
